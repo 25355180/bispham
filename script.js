87bf1fe75928513089819d4d9a549ba651687dc5
25355180 <147062677+25355180@users.noreply.github.com>
2025-03-10 14:18:49 +0000

    nav

---
+++ script.js
@@ -0,0 +1,8 @@
+function fixNav() {
+    var x = document.getElementById("navigation");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
