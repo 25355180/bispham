1b4450e76141978f944cc565384d8149202c38de
25355180 <147062677+25355180@users.noreply.github.com>
2025-03-12 12:58:32 +0000

    attempting php

---
+++ contactUs.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+The following people have contacted the club:
+<?php echo $_POST["yourtitle"]; ?><br>
+<?php echo $_POST["firstname"]; ?><br>
+<?php echo $_POST["surname"]; ?><br>
+<?php echo $_POST["yourphone"]; ?><br>
+<?php echo $_POST["youremail"]; ?><br>
+Their reason for contact:
+
+<?php echo $_POST["subject"]; ?><br>
+
+</body>
+</html>
